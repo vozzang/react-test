360f5be4ca4b3b2a323256a99a720f5cb8f53eee
vozzang <bunggae@gmail.com>
2022-06-29 10:14:59 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './shared/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
